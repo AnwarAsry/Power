bac26a3001ee1518643ae560192f1f4615d39264
AnwarAsry <anwareducation03@gmail.com>
2025-06-28 17:23:12 +0200

    header done but no menu

--- app/home.tsx
+++ app/home.tsx
@@ -2,7 +2,9 @@ import { IoIosSearch } from "react-icons/io";
 import type { Route } from "./+types/home";
 import logo from "../assets/power-logo.svg";
 import { FiShoppingCart, FiUser } from "react-icons/fi";
-import { IoMenu } from "react-icons/io5";
+import { IoLocationOutline, IoMenu } from "react-icons/io5";
+import { Headset } from "lucide-react";
+import { AmountIndicator } from "components/cart/AmountIndicator";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -13,33 +15,50 @@ export function meta({ }: Route.MetaArgs) {
 
 export default function Home() {
   return <>
-    <header className="w-full h-19.5 fixed top-2">
-      <div className="w-290 h-full mx-auto px-6 flex items-center rounded-lg bg-[#333]">
-        {/* Menu box */}
-        <div className="py-1 px-2 flex items-center rounded-md hover:cursor-pointer hover:bg-[#1f1f1f]">
-          <IoMenu className="size-9 text-white" />
-        </div>
-        {/* Logo box */}
-        <div className="w-32 h-fit mx-6">
-          <a href="/">
-            <img className="w-32 h-fit" src={logo} alt="" />
+    <header className="w-full fixed top-0">
+      <div className="w-300 mx-auto">
+        <div className="h-10 flex items-center">
+          <a className="px-3 flex items-center gap-1 transition hover:underline" href="/kundtjanst">
+            <Headset className="size-5 text-[#f86701]" />
+            <span>Kundtjänst</span>
           </a>
+          <div className="ml-3 flex-1">
+            <a className="px-3 first:border-r first:border-gray-300 transition hover:underline" href="/">Privatkund</a>
+            <a className="px-3 first:border-r first:border-gray-300 transition hover:underline" href="/foretag">Företagskund</a>
+          </div>
         </div>
-        {/* Search box */}
-        <div className="w-full max-w-120 py-2 px-4 flex rounded-full bg-white">
-          <input className="w-full pr-1 border-none outline-none" type="text" placeholder="Sök efter vad du letar efter" />
-          <IoIosSearch className="size-7 text-[#f86701]" />
-        </div>
-        {/* Buttons box */}
-        <div className="flex flex-1 justify-end gap-4">
-          <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-black text-white">
-            <span>MyPOWER</span>
-            <FiUser className="size-5" />
-          </a>
-          <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-[#f86701] text-white">
-            <FiShoppingCart className="size-5" />
-            <span className="min-w-[20px] h-[20px] px-1 flex items-center justify-center absolute -top-2 -right-2 rounded-full text-xs text-center bg-white text-black">3</span>
-          </a>
+        <div className="h-19.5 px-6 flex items-center rounded-lg bg-[#333]">
+          {/* Menu box */}
+          <div className="py-1 px-2 flex items-center rounded-md hover:cursor-pointer transition hover:bg-[#1f1f1f]">
+            <IoMenu className="size-9 text-white" />
+          </div>
+          {/* Logo box */}
+          <div className="w-32 h-fit mx-6">
+            <a href="/">
+              <img className="w-32 h-fit" src={logo} alt="" />
+            </a>
+          </div>
+          {/* Search box */}
+          <div className="w-full max-w-120 py-2 px-4 flex rounded-full bg-white">
+            <input className="w-full pr-1 border-none outline-none" type="text" placeholder="Sök efter vad du letar efter" />
+            <IoIosSearch className="size-7 text-[#f86701]" />
+          </div>
+          {/* Buttons box */}
+          <div className="flex flex-1 justify-end gap-4">
+            <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-black text-white transition hover:cursor-pointer">
+              <span className="text-sm">Hitta Butik</span>
+              <IoLocationOutline className="size-5 text-[#f86701]" />
+            </a>
+            <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-black text-white transition hover:cursor-pointer">
+              <span className="text-sm">MyPOWER</span>
+              <FiUser className="size-5 text-[#f86701]" />
+            </a>
+            <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-[#f86701] text-white transition hover:cursor-pointer hover:bg-[#df5d01]">
+              <span className="text-sm">Kundvagn</span>
+              <FiShoppingCart className="size-5" />
+              <AmountIndicator />
+            </a>
+          </div>
         </div>
       </div>
     </header>
---
+++ components/cart/AmountIndicator.tsx
@@ -0,0 +1,7 @@
+export const AmountIndicator = () => {
+    return (
+        <span className="min-w-[20px] h-[20px] px-1 flex items-center justify-center absolute -top-2 -right-2 rounded-full text-xs text-center bg-white text-black">
+            3
+        </span>
+    )
+}
