5a31761f28c1c41c5770c899b06c5e5857c16a62
AnwarAsry <anwareducation03@gmail.com>
2025-06-28 21:27:14 +0200

    responsive but search bar not push down

--- app/home.tsx
+++ app/home.tsx
@@ -15,9 +15,9 @@ export function meta({ }: Route.MetaArgs) {
 
 export default function Home() {
   return <>
-    <header className="w-full fixed top-0">
-      <div className="w-300 mx-auto">
-        <div className="h-10 flex items-center">
+    <header className="w-full sticky top-0">
+      <div className="w-full lg:w-250 xl:w-300 mt-0 lg:mt-3 xl:mt-0 mx-auto">
+        <div className="h-10 hidden items-center xl:flex">
           <a className="px-3 flex items-center gap-1 transition hover:underline" href="/kundtjanst">
             <Headset className="size-5 text-[#f86701]" />
             <span>Kundtjänst</span>
@@ -27,15 +27,15 @@ export default function Home() {
             <a className="px-3 first:border-r first:border-gray-300 transition hover:underline" href="/foretag">Företagskund</a>
           </div>
         </div>
-        <div className="h-19.5 px-6 flex items-center rounded-lg bg-[#333]">
+        <div className="h-19.5 px-3 lg:px-6 flex items-center lg:rounded-lg bg-[#333]">
           {/* Menu box */}
           <div className="py-1 px-2 flex items-center rounded-md hover:cursor-pointer transition hover:bg-[#1f1f1f]">
             <IoMenu className="size-9 text-white" />
           </div>
           {/* Logo box */}
-          <div className="w-32 h-fit mx-6">
+          <div className="w-32 h-full mx-6">
             <a href="/">
-              <img className="w-32 h-fit" src={logo} alt="" />
+              <img className="w-32 h-full" src={logo} alt="" />
             </a>
           </div>
           {/* Search box */}
@@ -46,15 +46,15 @@ export default function Home() {
           {/* Buttons box */}
           <div className="flex flex-1 justify-end gap-4">
             <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-black text-white transition hover:cursor-pointer">
-              <span className="text-sm">Hitta Butik</span>
+              <span className="text-sm hidden xl:block">Hitta Butik</span>
               <IoLocationOutline className="size-5 text-[#f86701]" />
             </a>
             <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-black text-white transition hover:cursor-pointer">
-              <span className="text-sm">MyPOWER</span>
+              <span className="text-sm hidden xl:block">MyPOWER</span>
               <FiUser className="size-5 text-[#f86701]" />
             </a>
             <a className="py-2 px-3 flex items-center gap-3 relative rounded-md bg-[#f86701] text-white transition hover:cursor-pointer hover:bg-[#df5d01]">
-              <span className="text-sm">Kundvagn</span>
+              <span className="text-sm hidden xl:block">Kundvagn</span>
               <FiShoppingCart className="size-5" />
               <AmountIndicator />
             </a>
@@ -62,8 +62,10 @@ export default function Home() {
         </div>
       </div>
     </header>
-    <div className="content-container">
-      <p className="bg-red-500">Welcome to the improved Power clone!</p>
+    <div className="content-grid-container">
+      <div className="content">
+        <p className="bg-red-500">Welcome to the improved Power clone!</p>
+      </div>
     </div>
   </>;
 }
